hanzoai/frappe

commit 4e302d001b9d0e71a8252b4ce5955e71985d39df

fix: task_id parameter for publish_progress
(cherry picked from commit e8eee1d0ec49bfa3d3817f8d0a8944abf50f22a4)

frappe/realtime.py
@@ -9,13 +9,16 @@ import frappe
 from frappe.utils.data import cstr
 
 
-def publish_progress(percent, title=None, doctype=None, docname=None, description=None):
+def publish_progress(
+	percent, title=None, doctype=None, docname=None, description=None, task_id=None
+):
 	publish_realtime(
 		"progress",
 		{"percent": percent, "title": title, "description": description},
 		user=None if doctype and docname else frappe.session.user,
 		doctype=doctype,
 		docname=docname,
+		task_id=task_id,
 	)
 
 
@@ -41,8 +44,11 @@ def publish_realtime(
 	if message is None:
 		message = {}
 
+	if not task_id and hasattr(frappe.local, "task_id"):
+		task_id = frappe.local.task_id
+
 	if event is None:
-		event = "task_progress" if frappe.local.task_id else "global"
+		event = "task_progress" if task_id else "global"
 	elif event == "msgprint" and not user:
 		user = frappe.session.user
 	elif event == "list_update":
@@ -50,9 +56,6 @@ def publish_realtime(
 		room = get_doctype_room(doctype)
 	elif event == "docinfo_update":
 		room = get_doc_room(doctype, docname)
-
-	if not task_id and hasattr(frappe.local, "task_id"):
-		task_id = frappe.local.task_id
 
 	if not room:
 		if task_id:

commit 0beeb4fd9b0db2d5ba2c375667295d159e1344e0

fix: formatting
Broke in 4e302d001b9d0e71a8252b4ce5955e71985d39df

Signed-off-by: Akhil Narang <me@akhilnarang.dev>

frappe/realtime.py
@@ -9,9 +9,7 @@ import frappe
 from frappe.utils.data import cstr
 
 
-def publish_progress(
-	percent, title=None, doctype=None, docname=None, description=None, task_id=None
-):
+def publish_progress(percent, title=None, doctype=None, docname=None, description=None, task_id=None):
 	publish_realtime(
 		"progress",
 		{"percent": percent, "title": title, "description": description},