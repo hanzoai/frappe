hanzoai/frappe

commit 6f5eb167c5ae3911580a878b514a83254935c2b0

fix: limit job count in RQ failed registry (backport #22162) (#22199)
* fix: limit job count in RQ failed registry (#22162)

* fix: limit job count in RQ failed registry

* chore: remove unnecessary test

This just checks if func is called with right values, which keep
changing as things evolve.
Everything is individually tested now so need for this test.

(cherry picked from commit c0ccf1ea266daa9a68fc97c301cae26c4004e3b7)

# Conflicts:
#	frappe/tests/test_background_jobs.py
#	frappe/utils/background_jobs.py

* chore: conflict and compatbility changes

---------

Co-authored-by: Ankush Menat <ankush@frappe.io>

frappe/utils/background_jobs.py
@@ -28,6 +28,7 @@ if TYPE_CHECKING:
 
 # TTL to keep RQ job logs in redis for.
 RQ_JOB_FAILURE_TTL = 7 * 24 * 60 * 60  # 7 days instead of 1 year (default)
+RQ_FAILED_JOBS_LIMIT = 1000  # Only keep these many recent failed jobs around
 RQ_RESULTS_TTL = 10 * 60
 
 
@@ -119,6 +120,7 @@ def enqueue(
 		"is_async": is_async,
 		"kwargs": kwargs,
 	}
+
 	if enqueue_after_commit:
 		if not frappe.flags.enqueue_after_commit:
 			frappe.flags.enqueue_after_commit = []
@@ -142,6 +144,7 @@ def enqueue(
 		failure_ttl=frappe.conf.get("rq_job_failure_ttl") or RQ_JOB_FAILURE_TTL,
 		result_ttl=frappe.conf.get("rq_results_ttl") or RQ_RESULTS_TTL,
 		job_id=job_id,
+		on_failure=truncate_failed_registry,
 	)
 
 
@@ -402,9 +405,9 @@ def get_redis_conn(username=None, password=None):
 		raise
 
 
-def get_queues() -> list[Queue]:
+def get_queues(connection=None) -> list[Queue]:
 	"""Get all the queues linked to the current bench."""
-	queues = Queue.all(connection=get_redis_conn())
+	queues = Queue.all(connection=connection or get_redis_conn())
 	return [q for q in queues if is_queue_accessible(q)]
 
 
@@ -470,3 +473,18 @@ def set_niceness():
 		nice_increment = cint(configured_niceness)
 
 	os.nice(nice_increment)
+
+
+def truncate_failed_registry(job, connection, type, value, traceback):
+	"""Ensures that number of failed jobs don't exceed specified limits."""
+	from frappe.utils import create_batch
+
+	conf = frappe.get_conf(site=job.kwargs.get("site"))
+	limit = (conf.get("rq_failed_jobs_limit") or RQ_FAILED_JOBS_LIMIT) - 1
+
+	for queue in get_queues(connection=connection):
+		fail_registry = queue.failed_job_registry
+		failed_jobs = fail_registry.get_job_ids()[limit:]
+		for job_ids in create_batch(failed_jobs, 100):
+			for job_obj in Job.fetch_many(job_ids=job_ids, connection=connection):
+				job_obj and fail_registry.remove(job_obj, delete_job=True)

frappe/core/doctype/rq_job/test_rq_job.py
@@ -9,13 +9,13 @@ from rq.job import Job
 
 import frappe
 from frappe.core.doctype.rq_job.rq_job import RQJob, remove_failed_jobs, stop_job
+from frappe.installer import update_site_config
 from frappe.tests.utils import FrappeTestCase, timeout
 from frappe.utils import cstr, execute_in_shell
 from frappe.utils.background_jobs import is_job_enqueued
 
 
 class TestRQJob(FrappeTestCase):
-
 	BG_JOB = "frappe.core.doctype.rq_job.test_rq_job.test_func"
 
 	@timeout(seconds=20)
@@ -103,6 +103,17 @@ class TestRQJob(FrappeTestCase):
 		job = frappe.enqueue(self.BG_JOB, sleep=10, job_id=job_id)
 		self.assertTrue(is_job_enqueued(job_id))
 		stop_job(job.id)
+
+	@timeout(20)
+	def test_clear_failed_jobs(self):
+		limit = 10
+		update_site_config("rq_failed_jobs_limit", limit)
+
+		jobs = [frappe.enqueue(method=self.BG_JOB, queue="short", fail=True) for _ in range(limit * 2)]
+		self.check_status(jobs[-1], "failed")
+		self.assertLessEqual(
+			RQJob.get_count({"filters": [["RQ Job", "status", "=", "failed"]]}), limit * 1.1
+		)
 
 
 def test_func(fail=False, sleep=0):

frappe/database/database.py
@@ -1355,6 +1355,7 @@ def enqueue_jobs_after_commit():
 		RQ_RESULTS_TTL,
 		execute_job,
 		get_queue,
+		truncate_failed_registry,
 	)
 
 	if frappe.flags.enqueue_after_commit and len(frappe.flags.enqueue_after_commit) > 0:
@@ -1367,6 +1368,7 @@ def enqueue_jobs_after_commit():
 				failure_ttl=frappe.conf.get("rq_job_failure_ttl") or RQ_JOB_FAILURE_TTL,
 				result_ttl=frappe.conf.get("rq_results_ttl") or RQ_RESULTS_TTL,
 				job_id=job.get("job_id"),
+				on_failure=truncate_failed_registry,
 			)
 		frappe.flags.enqueue_after_commit = []
 

frappe/tests/test_background_jobs.py
@@ -52,33 +52,6 @@ class TestBackgroundJobs(FrappeTestCase):
 		# lesser is earlier
 		self.assertTrue(high_priority_job.get_position() < low_priority_job.get_position())
 
-	def test_enqueue_call(self):
-		with patch.object(Queue, "enqueue_call") as mock_enqueue_call:
-			frappe.enqueue(
-				"frappe.handler.ping",
-				queue="short",
-				timeout=300,
-				kwargs={"site": frappe.local.site},
-			)
-
-			mock_enqueue_call.assert_called_once_with(
-				execute_job,
-				timeout=300,
-				kwargs={
-					"site": frappe.local.site,
-					"user": "Administrator",
-					"method": "frappe.handler.ping",
-					"event": None,
-					"job_name": "frappe.handler.ping",
-					"is_async": True,
-					"kwargs": {"kwargs": {"site": frappe.local.site}},
-				},
-				at_front=False,
-				failure_ttl=RQ_JOB_FAILURE_TTL,
-				result_ttl=RQ_RESULTS_TTL,
-				job_id=None,
-			)
-
 	def test_job_hooks(self):
 		self.addCleanup(lambda: _test_JOB_HOOK.clear())
 		with freeze_local() as locals, frappe.init_site(locals.site), patch(

commit 3bd89e52bd62ed38581c5f914748c1316004f4af

fix: import rq.Job correctly
`failed_job_registry` requires it
Broken since #22199

Signed-off-by: Akhil Narang <me@akhilnarang.dev>

frappe/utils/background_jobs.py
@@ -11,6 +11,7 @@ import redis
 from redis.exceptions import BusyLoadingError, ConnectionError
 from rq import Connection, Queue, Worker
 from rq.exceptions import NoSuchJobError
+from rq.job import Job
 from rq.logutils import setup_loghandlers
 from rq.worker import RandomWorker, RoundRobinWorker
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
@@ -22,10 +23,6 @@ from frappe.utils import cint, cstr, get_bench_id
 from frappe.utils.commands import log
 from frappe.utils.deprecations import deprecation_warning
 from frappe.utils.redis_queue import RedisQueue
-
-if TYPE_CHECKING:
-	from rq.job import Job
-
 
 # TTL to keep RQ job logs in redis for.
 RQ_JOB_FAILURE_TTL = 7 * 24 * 60 * 60  # 7 days instead of 1 year (default)