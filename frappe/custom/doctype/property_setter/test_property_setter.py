728ec07cbc31bb68f5db5d979b6756d3b7e31227
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-04-24 18:32:58 +0530

    test: Add test for bulk deletion of property setters
    (cherry picked from commit fdf74f32b9a4bded258d1be18b6c0020764158f3)
    
    # Conflicts:
    #	frappe/custom/doctype/property_setter/test_property_setter.py

--- frappe/custom/doctype/property_setter/test_property_setter.py
+++ frappe/custom/doctype/property_setter/test_property_setter.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: MIT. See LICENSE
+<<<<<<< HEAD
 from frappe.tests.utils import FrappeTestCase
 
 # test_records = frappe.get_test_records('Property Setter')
@@ -7,3 +8,51 @@ from frappe.tests.utils import FrappeTestCase
 
 class TestPropertySetter(FrappeTestCase):
 	pass
+=======
+import frappe
+from frappe.custom.doctype.property_setter.property_setter import (
+	bulk_delete_property_setters,
+)
+from frappe.tests import IntegrationTestCase
+
+
+class TestPropertySetter(IntegrationTestCase):
+	def test_bulk_delete_property_setters(self):
+		doctype = "ToDo"
+		fieldname = "status"
+
+		property_1 = "hidden"
+		property_2 = "no_copy"
+		properties = [property_1, property_2]
+
+		for property_name in properties:
+			frappe.make_property_setter(
+				{
+					"doctype": doctype,
+					"fieldname": fieldname,
+					"property": property_name,
+					"value": 1,
+					"property_type": "Check",
+				}
+			)
+
+		def property_setter_exists(property_name):
+			return frappe.db.exists(
+				"Property Setter",
+				{"doc_type": doctype, "field_name": fieldname, "property": property_name},
+			)
+
+		for property_name in properties:
+			self.assertTrue(property_setter_exists(property_name))
+
+		# 1
+		bulk_delete_property_setters(
+			[{"doctype": doctype, "fieldname": fieldname, "property": property_1}],
+			bypass_hooks=True,
+		)
+		self.assertFalse(property_setter_exists(property_1))
+
+		# 2
+		bulk_delete_property_setters([{"doc_type": doctype, "field_name": fieldname, "property": property_2}])
+		self.assertFalse(property_setter_exists(property_2))
+>>>>>>> fdf74f32b9 (test: Add test for bulk deletion of property setters)
