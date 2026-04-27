4839aa52add995faa45d400abd3321d4f76a9bf6
Shrihari Mahabal <shriharimahabal08@gmail.com>
2026-04-27 18:00:47 +0530

    fix: add perm check to document follow
    (cherry picked from commit ce3e72b889bb47d2f3ae3d3b00b6606350b1d8df)
    
    # Conflicts:
    #	frappe/desk/form/document_follow.py

--- frappe/desk/form/document_follow.py
+++ frappe/desk/form/document_follow.py
@@ -47,6 +47,9 @@ def follow_document(doctype, doc_name, user):
 	if (not frappe.get_meta(doctype).track_changes) or user == "Administrator":
 		return
 
+	if user != frappe.session.user and not frappe.has_permission("Document Follow", "write"):
+		frappe.throw(_("You can only follow documents for yourself."), frappe.PermissionError)
+
 	if not frappe.db.get_value("User", user, "document_follow_notify", ignore=True, cache=True):
 		return
 
@@ -58,7 +61,14 @@ def follow_document(doctype, doc_name, user):
 
 
 @frappe.whitelist()
+<<<<<<< HEAD
 def unfollow_document(doctype, doc_name, user):
+=======
+def unfollow_document(doctype: str, doc_name: str, user: str) -> bool:
+	if user != frappe.session.user:
+		frappe.throw(_("You can only unfollow documents for yourself."), frappe.PermissionError)
+
+>>>>>>> 56c602e94d (fix: add perm check to document follow)
 	doc = frappe.get_all(
 		"Document Follow",
 		filters={"ref_doctype": doctype, "ref_docname": doc_name, "user": user},
