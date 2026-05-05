c1c601578cbfafc15743ba5798e1615dbec6885c
wreckage0907 <girish.raghav2004@gmail.com>
2026-05-05 20:51:46 +0530

    fix: document follow unfollow broken due to PEP 563 lazy annotations bypassing type coercion

--- frappe/desk/form/document_follow.py
+++ frappe/desk/form/document_follow.py
@@ -17,10 +17,12 @@ if TYPE_CHECKING:
 
 @frappe.whitelist()
 def update_follow(doctype: str, doc_name: str, following: bool):
+	following = frappe.utils.sbool(following)
 	if following:
 		return (follow_document(doctype, doc_name, frappe.session.user) and True) or False
 	else:
-		return unfollow_document(doctype, doc_name, frappe.session.user)
+		unfollow_document(doctype, doc_name, frappe.session.user)
+		return False
 
 
 @frappe.whitelist()
@@ -89,7 +91,7 @@ def unfollow_document(doctype: str, doc_name: str, user: str) -> bool:
 	if doc:
 		frappe.delete_doc("Document Follow", doc[0].name, force=True)
 		frappe.toast(_("Un-following document {0}").format(doc_name))
-		return False
+		return True
 	return False
 
 
