c81e689d4fdb556f053ca22b36c5c0600dec9eb1
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-06-30 09:18:00 +0530

    fix(multicheck): avoid per-checkbox change events on select all
    (cherry picked from commit fc25c7c7382e7a7f4e5db99c75cd571012d78d18)
    
    # Conflicts:
    #	frappe/public/js/frappe/form/controls/multicheck.js

--- frappe/public/js/frappe/form/controls/multicheck.js
+++ frappe/public/js/frappe/form/controls/multicheck.js
@@ -130,7 +130,21 @@ frappe.ui.form.ControlMultiCheck = class ControlMultiCheck extends frappe.ui.for
 	}
 
 	select_all(deselect = false) {
+<<<<<<< HEAD
 		$(this.wrapper).find(`:checkbox`).prop("checked", deselect).trigger("click");
+=======
+		this.selected_options = [];
+		this.options.forEach((option) => {
+			const checkbox = option.$checkbox.find(":checkbox").get(0);
+			if (!checkbox.disabled) {
+				checkbox.checked = !deselect;
+			}
+			if (checkbox.checked) {
+				this.selected_options.push(option.value);
+			}
+		});
+		this.df.on_change && this.df.on_change();
+>>>>>>> fc25c7c738 (fix(multicheck): avoid per-checkbox change events on select all)
 	}
 
 	select_options(selected_options) {
